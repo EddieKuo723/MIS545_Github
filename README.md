18ccb5ccc334c6b6acdade9dad35ff6fd93b65e7
EddieKuo723 <66857463+EddieKuo723@users.noreply.github.com>
2021-10-27 23:19:04 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+## First step
+Apply for personal [access tokens](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token)
+
+## Building
+
+Run `pip install -r requirements.txt` 
+
+## Change access token
+
+Change access_token in `crawl_github.py` to personal access token on line 5
+
+## Execute
+
+`python crawl_github.py`
+
+## Library
+
+- [PyGithub](https://pygithub.readthedocs.io/en/latest/introduction.html)
+
+
+## License
+
+This project is released under the Apache license 2.0
+For more details, take a look at the [LICENSE](LICENSE) file.
---
+++ crawl_github.py
@@ -0,0 +1,29 @@
+
+from github import Github
+
+# using an access token
+g = Github("access_token")
+
+# repositories = g.search_repositories(query='iot')
+
+#example
+repositories = g.search_repositories(query='kevinwlu/iot')
+
+for repo in repositories:
+	print(repo)
+	# print(repo.get_topics())
+	
+	contents = repo.get_contents("")
+	while contents:
+		file_content = contents.pop(0)
+		if file_content.type == "dir":
+			contents.extend(repo.get_contents(file_content.path))
+		else:
+			if '.py' not in file_content.path:
+				continue
+			print(file_content)
+			print(file_content.path)
+
+			# real code in here
+			# print(file_content.decoded_content.decode())
+	break
