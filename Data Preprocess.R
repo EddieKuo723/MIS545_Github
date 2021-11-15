46111555545f802bd51308cb1a73e889583b235e
EddieKuo723 <66857463+EddieKuo723@users.noreply.github.com>
2021-11-14 18:14:48 -0700

    Create Data Preprocess.R

---
+++ Data Preprocess.R
@@ -0,0 +1,38 @@
+# MIS 545 Section 02
+
+# Install the required packages
+# install.packages("tidyverse")
+
+
+# Load the tidyverse and neuralnet libraries
+library("tidyverse")
+
+# Set the working directory to your Lab12 folder
+setwd("C:/Users/user/Documents/GitHub/MIS545_Github_CyberSecurity")
+
+# Read Github_most_stars.csv into a tibble called githubStar
+githubStar <- read_csv(file="Github_most_stars.csv",
+                           col_types = "cccnnnnnncc",
+                           col_names = TRUE)
+# Display githubStar in the console
+print(githubStar)
+
+# Display the structure of githubStar in the console
+# Display the summary of githubStar in the console
+print(str(githubStar))
+print(summary(githubStar))
+
+
+githubStar <- githubStar %>% 
+  select(stars,forks_count, issue_count,network_count,
+         subscribers_count, watchers_count
+      )
+
+githubStar <- githubStar%>%
+  mutate(isTarget = ifelse(stars > median(stars),TRUE,FALSE))
+
+
+
+
+
+
