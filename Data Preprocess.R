6a25c25860afb85b5b585ceb155b1ea9407e28f6
Rahul Shakya <rahul.shakya3@gmail.com>
2021-11-17 14:51:31 -0700

    Scaled Data to Alpha

--- Data Preprocess.R
+++ Data Preprocess.R
@@ -8,7 +8,7 @@
 library("tidyverse")
 
 # Set the working directory to your Lab12 folder
-setwd("C:/Users/user/Documents/GitHub/MIS545_Github_CyberSecurity")
+# setwd("C:/Users/user/Documents/GitHub/MIS545_Github_CyberSecurity")
 
 # Read Github_most_stars.csv into a tibble called githubStar
 githubStar <- read_csv(file="Github_most_stars.csv",
@@ -23,18 +23,49 @@ print(str(githubStar))
 print(summary(githubStar))
 
 
-githubStar <- githubStar %>% 
-  select(stars,forks_count, issue_count,network_count,
-         subscribers_count, watchers_count
+# githubStarStat <- githubStar %>% 
+#   select(stars,forks_count, issue_count,network_count,
+#          subscribers_count, watchers_count
+#       )
+
+# githubStar <- githubStar%>%
+#   mutate(isTarget = 
+#            ifelse((stars*0.5 + forks_count*0.3 + subscribers_count * 0.2) >
+#                   mean(stars*0.5 + forks_count*0.3+ subscribers_count * 0.2),
+#                   TRUE,FALSE))
+
+githubStar <- githubStar %>%
+  mutate(starsScaled = 
+           ((stars - min(stars))
+             /(max(stars) - min(stars))) * (1 - 0) + 0
+         )
+
+githubStar <- githubStar %>%
+  mutate(forksCountScaled = 
+           ((forks_count - min(forks_count))
+            /(max(forks_count) - min(forks_count))) * (1 - 0) + 0
+        )
+
+githubStar <- githubStar %>%
+  mutate(networkCountScaled = 
+           ((network_count - min(network_count))
+            /(max(network_count) - min(network_count))) * (1 - 0) + 0
+        )
+
+githubStar <- githubStar %>%
+  mutate(suscribersCountScaled = 
+           ((subscribers_count - min(subscribers_count))
+            /(max(subscribers_count) - min(subscribers_count))) * (1 - 0) + 0
       )
 
-githubStar <- githubStar%>%
-  mutate(isTarget = 
-           ifelse((stars*0.5 + forks_count*0.3 + subscribers_count * 0.2) >
-                  mean(stars*0.5 + forks_count*0.3+ subscribers_count * 0.2),
-                  TRUE,FALSE))
+githubStar <- githubStar %>%
+  mutate(watchersCountScaled = 
+           ((watchers_count - min(watchers_count))
+            /(max(watchers_count) - min(watchers_count))) * (1 - 0) + 0
+  )
 
-summary(githubStar)
+
+print(githubStar)
 
 
 
